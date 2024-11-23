f7626e3fdfceca764a279e540be6665d9cd1e99f
Emily Skelley <ESkelley0673@students.cptc.edu>
2024-11-23 13:42:43 -0800

    Add project files.

---
+++ eskelTestSite/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using eskelTestSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eskelTestSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
